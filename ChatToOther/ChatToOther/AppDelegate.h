86e52e62bd33926896c8385ca017cb9d85aec3c2
kongzhaozhuang <kongzhaozhuang@allinmd.cn>
2016-04-25 09:22:29 +0800

    init

---
+++ ChatToOther/ChatToOther/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatToOther
+//
+//  Created by allin on 16/4/25.
+//  Copyright © 2016年 allin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatToOther/ChatToOther/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatToOther
+//
+//  Created by allin on 16/4/25.
+//  Copyright © 2016年 allin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
